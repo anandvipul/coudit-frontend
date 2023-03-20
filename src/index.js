9998750b663d1245f5ff01f5e65bb59dd0191b7a
anandvipul <anandvipul01@gmail.com>
2023-03-20 23:35:50 +0530

    Sign In/Up

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Container from "./Components/Container";
+import SignIn from "./Components/Pages/SignIn";
+import SignUp from "./Components/Pages/SignUp";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Container />
+    {/* <Container /> */}
+    <SignUp />
   </React.StrictMode>
 );
 
