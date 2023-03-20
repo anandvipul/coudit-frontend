6b91bb063290f2aed3f15ba49be30281c9beabc2
anandvipul <anandvipul01@gmail.com>
2023-03-20 23:35:10 +0530

    SignIn

---
+++ src/Components/Pages/SignIn.js
@@ -0,0 +1,37 @@
+import Header from "../Elements/Header";
+import Footer from "../Elements/Footer";
+
+export default function SignIn(props) {
+  return (
+    <>
+      <Header />
+
+      <section className="center sign-in">
+        <h1 style={{ marginBottom: 0, marginTop: "3rem" }}>Sign in</h1>
+        <p style={{ marginBottom: "2rem" }}>need an account ?</p>
+        <div className="signin-container center">
+          <form>
+            <input
+              className="input-field"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+            <input
+              className="input-field"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+            <button type="submit" className="signin-btn">
+              Sign in
+            </button>
+          </form>
+        </div>
+      </section>
+      <div className="footer-container">
+        <Footer />
+      </div>
+    </>
+  );
+}
