2df01f8b3d8c4f1e7aa9b99ba41a69938e9861e8
anand <anandvipul01@gmail.com>
2023-04-29 08:22:46 +0530

    refactor

--- src/Components/LoaderScreen.js
+++
@@ -1,9 +0,0 @@
-function LoaderScreen(props) {
-  return (
-    <div className="loading">
-      <img src="/loading2.gif" alt="loading" />
-    </div>
-  );
-}
-
-export default LoaderScreen;
--- src/CustomContext/dataContext.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-let DataContext = React.createContext({});
-
-let DataProvider = DataContext.Provider;
-let DataConsumer = DataContext.Consumer;
-
-export { DataConsumer, DataProvider };
