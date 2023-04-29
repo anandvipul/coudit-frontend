93beb62eb02ea1585334fe70c3130ed88de26522
Vipul Anand <45119902+anandvipul@users.noreply.github.com>
2023-04-29 07:36:57 +0530

    Revert "V0.3 develop"

--- src/v0_3/Pages/LoaderScreen.js
+++
@@ -1,9 +0,0 @@
-function LoaderScreen(props) {
-  return (
-    <div className="loading">
-      <img src="/transparent-loader.gif" alt="loading" />
-    </div>
-  );
-}
-
-export default LoaderScreen;
